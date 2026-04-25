c1606c8a0548df4823ae4444c19ba39467283e8b
Guilherme Caon <caon.guilhermel@gmail.com>
2026-04-24 22:09:45 -0300

    URGENTE: Correcao definitiva de tipagem e limpeza de cache

--- .next/dev/static/chunks/app_page_tsx_12uwdpq._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0fosqnl._.js","static/chunks/_067ybjh._.js"],
-    source: "dynamic"
-});
