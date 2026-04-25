9d506a3c91b9c0d525ae7f4b69ecde64a992ff61
caon-guilherme <caon.guilherme@gmail.com>
2026-04-24 23:00:52 -0300

    Build: Mapeando Commit SHA automaticamente no next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA || 'LOCAL-DEV',
+  },
 };
+
 
 export default nextConfig;
